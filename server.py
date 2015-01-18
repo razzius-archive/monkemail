a2d315b4cae7f282e17a479c8cdfcd377b2b8a7c
Razzi Abuissa <razzi53@gmail.com>
2015-01-18 08:08:43 -0500

    Disable debug

--- server.py
+++ server.py
@@ -242,4 +242,4 @@ def initdb():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', debug=True, port=int(os.environ.get('PORT', 5000)))
+    app.run('0.0.0.0', port=int(os.environ.get('PORT', 5000)))
