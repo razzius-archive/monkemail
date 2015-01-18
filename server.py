68c56faf0045eb6e353b3f897380cdea09983cec
Razzi Abuissa <razzi53@gmail.com>
2015-01-17 22:29:33 -0500

    Finish frontend and hook up database

--- server.py
+++ server.py
@@ -2,24 +2,66 @@ import os
 import urlparse
 
 from flask import Flask, make_response, redirect, request
+from flask.ext.sqlalchemy import SQLAlchemy
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import relationship
 
 import requests
 from github import Github
 
 
+MANDRILL_API_KEY = os.environ.get('MANDRILL_API_KEY')
 GITHUB_CLIENT_ID = os.environ.get('GITHUB_CLIENT_ID')
 GITHUB_CLIENT_SECRET = os.environ.get('GITHUB_CLIENT_SECRET')
 
 GITHUB_OAUTH_ENDPOINT = 'https://github.com/login/oauth/access_token'
 
 app = Flask('Monkemail')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+
+
+
+### Database Bizness ###
+db = SQLAlchemy(app)
+
+
+class BaseMixin(object):
+    """Defines an id column and default tablename for database models."""
+    id = Column(Integer, primary_key=True)
+
+    @declared_attr
+    def __tablename__(self):
+        """ Set default tablename."""
+        return self.__name__.lower()
+
+
+class User(db.Model, BaseMixin):
+    """Represents a user who has signed up for this service using their Github."""
+    email = Column(String(120), unique=True)
+    github_api_token = Column(String(50), unique=True)
+
+    def __init__(self, email, github_api_token):
+        self.email = email
+        self.github_api_token = github_api_token
+
+
+
+class Website(db.Model, BaseMixin):
+    """Represents a website that a user owns which can be integrated with Monkemail."""
+    contact_email = Column(String(120), unique=True)
+    url = Column(String(255))
+    owner_id = Column(Integer, ForeignKey('user.id'), index=True)
+    owner = relationship('User', foreign_keys=[owner_id])
+
 
 def get_primary_github_email(github_client):
     """Get a Github user's primary email."""
     user_emails = github_client.get_user().get_emails()
 
     for email_dict in user_emails:
-        __import__('ipdb').set_trace()
         if email_dict['primary']:
             return email_dict['email']
 
@@ -51,6 +93,17 @@ def oauth():
     github = Github(token)
     user_email = get_primary_github_email(github)
 
+    # Save the user
+
+    new_user = User(user_email, token)
+    db.session.add(new_user)
+
+    try:
+        db.session.commit()
+    except IntegrityError:
+        # They're already in the database
+        pass
+
     redirect_url = 'http://monkemail.me/login'
 
     response = make_response(redirect(redirect_url))
@@ -58,6 +111,11 @@ def oauth():
     response.set_cookie('user_email', user_email)
 
     return response
+
+
+@app.cli.command()
+def initdb():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.environ.get('PORT', 8000))
